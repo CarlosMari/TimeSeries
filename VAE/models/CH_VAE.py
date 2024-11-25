89078fb60cabb44dd1b95b07420e5fc3472ab071
Carlos Marí <202009279@alu.comillas.edu>
2024-11-25 09:26:52 +0100

    Different Activation Support

--- VAE/models/CH_VAE.py
+++ VAE/models/CH_VAE.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 from torch.nn import functional as F
 
+ACTIVATION = nn.GELU()
 
 class CHVAE(nn.Module):
     def __init__(self, config) -> None:
@@ -11,59 +12,57 @@ class CHVAE(nn.Module):
 
         self.encoder = nn.Sequential(
             nn.Conv1d(in_channels=config['in_channels'], out_channels=16, kernel_size=7, stride=2, padding=3),
-            nn.LeakyReLU(),
+            ACTIVATION,
             nn.Conv1d(in_channels=16, out_channels=16, kernel_size=5, stride=2, padding=2),
-            nn.LeakyReLU(),
+            ACTIVATION,
             nn.Conv1d(in_channels=16, out_channels=8, kernel_size=5, stride=2, padding=2),
-            nn.LeakyReLU(),
+            ACTIVATION,
             nn.Conv1d(in_channels=8, out_channels=8, kernel_size=3, stride=2, padding=1),
-            nn.LeakyReLU(),
-            nn.Conv1d(in_channels=8, out_channels=4, kernel_size=3, stride=1, padding=1),
-            nn.LeakyReLU(),
+            ACTIVATION,
+            nn.Conv1d(in_channels=8, out_channels=8, kernel_size=3, stride=1, padding=1),
+            ACTIVATION,
         )
 
-        # Mean mapping
-
         self.corr = torch.nn.Sequential(
-            torch.nn.Linear(36, 2*config['latent_dim']),
-            torch.nn.LeakyReLU(),
+            torch.nn.Linear(36*2, 2*config['latent_dim']),
+            ACTIVATION,
         )
 
 
         self.mean_map = torch.nn.Sequential(
-            torch.nn.Linear(2*config['latent_dim'], 24),
-            torch.nn.LeakyReLU(),
-            torch.nn.Linear(24, 24),
-            torch.nn.LeakyReLU(),
-            torch.nn.Linear(24, config["latent_dim"])
+            torch.nn.Linear(2*config['latent_dim'], 40),
+            ACTIVATION,
+            torch.nn.Linear(40, 40),
+            ACTIVATION,
+            torch.nn.Linear(40, config["latent_dim"])
         )
 
         self.std_map = torch.nn.Sequential(
-            torch.nn.Linear(2*config['latent_dim'], 24),
-            torch.nn.LeakyReLU(),
-            torch.nn.Linear(24, 24),
-            torch.nn.LeakyReLU(),
-            torch.nn.Linear(24, config["latent_dim"])
+            torch.nn.Linear(2*config['latent_dim'], 40),
+            ACTIVATION,
+            torch.nn.Linear(40, 40),
+            ACTIVATION,
+            torch.nn.Linear(40, config["latent_dim"])
         )
 
         self.linear2 = torch.nn.Sequential(
-            torch.nn.Linear(config["latent_dim"], 24),
-            torch.nn.LeakyReLU(),
-            torch.nn.Linear(24, 24),
-            torch.nn.LeakyReLU(),
-            torch.nn.Linear(24, 18),
-            torch.nn.LeakyReLU(),
+            torch.nn.Linear(config["latent_dim"], config["latent_dim"]),
+            ACTIVATION,
+            torch.nn.Linear(config["latent_dim"], config["latent_dim"]*2),
+            ACTIVATION,
+            torch.nn.Linear(config["latent_dim"]*2, 36*2),
+            ACTIVATION,
         )
 
         self.decoder = torch.nn.Sequential(
-            torch.nn.ConvTranspose1d(in_channels = 2, out_channels = 8, kernel_size = 3, padding=1),
-            torch.nn.LeakyReLU(),
+            torch.nn.ConvTranspose1d(in_channels = 8, out_channels = 8, kernel_size = 3, padding=1),
+            ACTIVATION,
             torch.nn.ConvTranspose1d(in_channels = 8, out_channels = 8, kernel_size = 3, stride=2, padding=1),
-            torch.nn.LeakyReLU(),
+            ACTIVATION,
             torch.nn.ConvTranspose1d(in_channels = 8, out_channels = 16, kernel_size = 5, stride=2, padding=2),
-            torch.nn.LeakyReLU(),
+            ACTIVATION,
             torch.nn.ConvTranspose1d(in_channels = 16, out_channels = 16,  kernel_size = 5, stride=2, padding=2),
-            torch.nn.LeakyReLU(),
+            ACTIVATION,
             torch.nn.ConvTranspose1d(in_channels = 16, out_channels = config["in_channels"], kernel_size=7, stride=2, padding=3),
         )
 
@@ -92,10 +91,8 @@ class CHVAE(nn.Module):
         
         # Now pass the information through the convolutional feature extracto
         pre_code = self.encoder(X)
-                
         # Reshape the tensor dimension for latent space sampling
         
-
         B, C, L = pre_code.shape[0], pre_code.shape[1], pre_code.shape[2]
         flattened = pre_code.view(B,C*L)
         flattened = self.corr(flattened)        
@@ -106,10 +103,11 @@ class CHVAE(nn.Module):
         code = self.sample(mu, log_var)
         
         post_code = self.linear2(code)
-        
-        
-        X_hat = self.decoder( post_code.view(B,C,L)).view(shape)
-                          
+
+        X_hat = self.decoder( post_code.view(B,C,L))
+
+
+        X_hat = X_hat.view(shape)
 
         return X_hat, code, mu, log_var
     
@@ -132,4 +130,4 @@ class CHVAE(nn.Module):
         SSL = F.mse_loss(x_hat[:,:,-1], x[:,:,-1])
         # ICL = F.mse_loss(x_hat[:,:,0], x[:,:,0])
 
-        return BCE + alpha*KLD + gamma*SS
+        return BCE + alpha*KLD + gamma*SSL
